96094f61489f6016f2b068db161f9964f3d5c267
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-05-12 12:59:15 +0300

    implemented people also bought which is fetch recommendations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,8 +19,10 @@ function App() {
   }, [checkAuth]);
 
   useEffect(() => {
+    if (!user) return;
+
     getCartItems();
-  }, [getCartItems]);
+  }, [getCartItems, user]);
 
   if (checkingAuth) return <LoadingSpinner />;
 
